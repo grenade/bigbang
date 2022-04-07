0a1bcd3d2baf13c432a2d199d30a4f75ecf08937
Rob Thijssen <rthijssen@gmail.com>
2022-04-07 05:42:57 +0300

    wip: scaffolding

--- src/App.js
+++ src/App.js
@@ -1,24 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Form from 'react-bootstrap/Form';
+import Tabs from 'react-bootstrap/Tabs';
+import Tab from 'react-bootstrap/Tab';
+
+import YAML from 'yaml';
+
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+
+const baseSpecs = [
+  {
+    name: 'polkadot',
+  },
+  {
+    name: 'kusama',
+  },
+  {
+    name: 'rococo',
+  },
+  {
+    name: 'westend',
+  },
+];
 
 function App() {
+  const [config, setConfig] = useState({
+    base: 'westend',
+  });
+  const [spec, setSpec] = useState({});
+
+  useEffect(() => {
+    if (!!config.base) {
+      fetch(`${window.location.protocol}//${window.location.host}/spec/${config.base}.json`)
+        .then((response) => response.json())
+        .then((spec) => {
+          setSpec(spec);
+        })
+        .catch(console.error);
+    }
+    return () => {};
+  }, [config.base]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Form>
+        <Row>
+          <Col>
+            <Form.Group>
+              <Form.Label>
+                base
+              </Form.Label>
+              <Form.Select
+                defaultValue={config.base}
+                onChange={
+                  (e) => {
+                    const base = e.target.value;
+                    setConfig((c) => ({
+                      ...c,
+                      base
+                    }))
+                  }
+                }>
+                {
+                  baseSpecs.map((baseSpec, bsI) => (
+                    <option key={bsI} value={baseSpec.name}>
+                      {baseSpec.name}
+                    </option>
+                  ))
+                }
+              </Form.Select>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>address</Form.Label>
+              <Form.Control type="text" placeholder={`${window.location.protocol}//${window.location.host}/spec/${config.base}.json`} />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group>
+              <Form.Label>chainspec</Form.Label>
+              <Tabs defaultActiveKey="json">
+                {['json', 'yaml'].map((markup, mI) => (
+                  <Tab key={mI} eventKey={markup} title={markup}>
+                    <SyntaxHighlighter language={markup} style={docco}>
+                      {(markup === 'json') ? JSON.stringify(spec, null, 2) : YAML.stringify(spec)}
+                    </SyntaxHighlighter>
+                  </Tab>
+                ))}
+              </Tabs>
+            </Form.Group>
+          </Col>
+        </Row>
+      </Form>
+    </Container>
   );
 }
 
